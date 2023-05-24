45779b7c2a1e27e5099a27b8151ec76e9f8fa44f
Mahmoud Mousa <Mahmoud.m@covergenius.com>
2023-05-24 17:15:49 +0900

    Add CORS Support

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ from typing import Union
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+
+origins = [
+    "https://vite-test-fm8k.onrender.com/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
